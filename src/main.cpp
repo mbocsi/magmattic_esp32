fb9c485f0f401f5883cefd2706cc6a8853879859
Marton Bocsi <mvbocsi@gmail.com>
2025-03-12 17:55:03 -0500

    Connected ws client to server

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,32 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ArduinoWebsockets.h>
+
+using namespace websockets;
 
 const char * ssid = (char *) "UWNet";
 const char * password = (char *) "";
+const char * websocket_addr = "ws://10.140.250.213:44444";
+
+WebsocketsClient client;
 
 // put function declarations here:
 void initWifi();
+void scanWifi();
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   initWifi(); 
+  client.connect(websocket_addr);
+  Serial.println("Connected to WS server");
+  client.send("{\"type\":\"esp32_init\"}");
   Serial.println("Setup done");
 }
 
 void loop() {
-  Serial.println("scan start");
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-    Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
-    }
-  }
-  Serial.println("");
+  // scanWifi();
+  // client.send("Sending voltages!");
   delay(5000);
 }
 
@@ -60,4 +51,28 @@ void initWifi() {
     delay(1000);
   }
   Serial.println(WiFi.localIP());
+}
+
+void scanWifi() {
+  Serial.println("scan start");
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+    Serial.println("no networks found");
+  } else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
 }
