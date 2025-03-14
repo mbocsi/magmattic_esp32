5aa4aa134b8fd14725eaa0999f9121dd19e0be80
Marton Bocsi <mvbocsi@gmail.com>
2025-03-14 18:40:55 -0500

    Added code for ADC

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,83 @@
 #include <Arduino.h>
+#include <math.h>
 #include <WiFi.h>
 #include <ArduinoWebsockets.h>
+#include <ads1262.h>
+#include <SPI.h>
+
+// TODO: Define pins
+// #define ADS1262_DRDY_PIN = 0;
+// #define ADS1262_CS_ING = 0;
+// #define ADS1262_START_PIN = 0;
+// #define ADS1262_PWDN = 0;
 
 using namespace websockets;
 
-const char * ssid = (char *) "UWNet";
-const char * password = (char *) "";
-const char * websocket_addr = "ws://10.140.250.213:44444";
+const char * SSID = (char *) "UWNet";
+const char * PASSWORD = (char *) "";
+const char * WEBSOCKET_ADDR = "ws://10.140.187.6:44444";
+const double VREF = 2.5;
+const double ADC_RESOLUTION = (double)(VREF/pow(2,31));
+char * spi_rx_buff_ptr;
+signed long adc_reading = 0;
+double adc_voltage;
+int buf_size = 32; // Samples
+int sample_rate = 1000; // Hz
 
 WebsocketsClient client;
+ads1262 adc;
 
-// put function declarations here:
 void initWifi();
-void scanWifi();
+void recv(WebsocketsMessage message);
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+  delay(200);
+  // Connect Wifi
   initWifi(); 
-  client.connect(websocket_addr);
-  Serial.println("Connected to WS server");
-  client.send("{\"type\":\"esp32_init\"}");
+  delay(100);
+  // Attach recv handler
+  client.onMessage([&](WebsocketsMessage message) {
+    recv(message);
+  });
+  // Connect WS client 
+  client.connect(WEBSOCKET_ADDR);
+  if(client.available()) {
+    Serial.println("connected to WS server");
+  } else {
+    Serial.println("failed to connect to WS server");
+  }
+  // Send subscription message to server for commands to ESP32
+  client.send("{\"topic\":\"subscribe\",\"payload\":{\"topics\":[\"adc/command\"]}}");
+  // TODO: Initialize pins
+  // pinMode(ADS1262_DRDY_PIN, INPUT);                  //data ready input line
+  // pinMode(ADS1262_CS_PIN, OUTPUT);                   //chip enable output line
+  // pinMode(ADS1262_START_PIN, OUTPUT);               // start 
+  // pinMode(ADS1262_PWDN_PIN, OUTPUT);                // Power down output  
+  // Initialize ADC
+  adc.ads1262_Init();
   Serial.println("Setup done");
 }
 
 void loop() {
-  // scanWifi();
-  // client.send("Sending voltages!");
-  delay(5000);
+  if (!client.available()) {
+    Serial.println("ws server disconnected - retrying connection...");
+    client.connect(WEBSOCKET_ADDR);
+  }
+  if((digitalRead(ADS1262_DRDY_PIN)) == LOW)        // monitor Data ready(DRDY pin)
+  {  
+    spi_rx_buff_ptr = adc.ads1262_Read_Data();      // read 6 bytes conversion register
+    adc_reading = (signed long) ((unsigned long)spi_rx_buff_ptr[0]) << 24  // Extract value from buffer
+      | ((unsigned long)spi_rx_buff_ptr[1]) << 16 
+      | ((unsigned long)spi_rx_buff_ptr[2]) << 8 
+      | ((unsigned long)spi_rx_buff_ptr[3]);
+    adc_voltage = ADC_RESOLUTION * adc_reading; // Convert to volts
+  } else {
+    Serial.println("Data was not ready!");
+  }
+  Serial.printf("Voltage Reading: %lfV\n", adc_voltage);
+  client.ping();
+  delay(1000);
 }
 
 // put function definitions here:
@@ -44,7 +94,7 @@ void initWifi() {
   Serial.printf("%02X\n", baseMac[5]);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
@@ -53,26 +103,7 @@ void initWifi() {
   Serial.println(WiFi.localIP());
 }
 
-void scanWifi() {
-  Serial.println("scan start");
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-    Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
-    }
-  }
-  Serial.println("");
-}
+void recv(WebsocketsMessage message) {
+  Serial.print("recv - ");
+  Serial.println(message.data());
+};
